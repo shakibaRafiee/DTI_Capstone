33e13d2ad4c9e4bad9c5a2e705357a650b6eb9d8
ShakibaRafiee <shrafiee@umd.edu>
2022-05-08 21:56:15 -0700

    updated App_git_01 to trouble shoot streamlit interface

--- App/App_git_01.py
+++ App/App_git_01.py
@@ -1,3 +1,11 @@
 import streamlit as st
+import dill
 
 st.title('RunLikeU')
+
+@st.cache(allow_output_mutation=True)
+def load_graph():
+    graph = dill.load(open('../San_Francisco/temp_graph.pkd', 'rb'))
+    return graph
+
+graph = load_graph()
